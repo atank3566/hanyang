eb909133fbc78ca8cb95ba1f23d68274898fcd47
Park heewoong <90432570+atank3566@users.noreply.github.com>
2022-12-10 17:55:24 +0900

    Update step1code.c

---
+++ .vscode/robotc/step1code.c
@@ -0,0 +1,528 @@
+#pragma config(Sensor, S1, c1, sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S2, c2, sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Sensor, S3, c3, sensorEV3_Color, modeEV3Color_Color)
+#pragma config(Motor, motorB, lm, tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor, motorC, rm, tmotorEV3_Large, PIDControl, encoder)
+
+#define DIFF 10
+
+int nMotorSpeedSetting = 35, vertex = 0, count = 0, row = 0, val, color, init = 0,
+r = 0, c = 0, i, j, f, e, score;
+
+int S[4][4], dt[4][4];
+
+
+void colorSearchTurn()
+{
+      setMotorSpeed(lm, 15);
+      setMotorSpeed(rm, -15);
+      sleep(120);
+      setMotorSpeed(lm, 0);
+      setMotorSpeed(rm, 0);
+      sleep(50);
+}
+
+void patchSave()
+{
+   int i = 0, color = 0;
+   setMotorSpeed(lm, 0);
+   setMotorSpeed(rm, 0);
+
+   color = getColorName(c2);
+
+   if (init == 0 ){
+         displayBigTextLine(1, "S[%d][%d] = YELLOW", row , count);
+         S[row][count] = 0;
+         sleep(1000);
+         init++;
+   }
+   else if (row % 2 == 0){
+      if(color == 3){
+         displayBigTextLine(1,"S[%d][%d] = GREEN", row, ++count);
+         S[row][count] = -1;
+         sleep(1000);
+      }
+      else if (color == 5){
+         displayBigTextLine(1, "S[%d][%d] = RED", row, ++count);
+         S[row][count] = 1;
+         sleep(1000);
+      }
+      else{
+         displayBigTextLine(1, "S[%d][%d] = YELLOW", row , ++count);
+         S[row][count] = 0;
+         sleep(1000);
+      }
+   }
+
+   else if(row % 2!= 0){
+      if(color == 3){
+         displayBigTextLine(1,"S[%d][%d] = GREEN", row, 3-(++count));
+         S[row][3-count] = -1;
+         sleep(1000);
+      }
+      else if(color == 5){
+         displayBigTextLine(1,"S[%d][%d] = RED", row, 3-(++count));
+         S[row][3-count] = 1;
+         sleep(1000);
+      }
+      else{
+         displayBigTextLine(1,"S[%d][%d] = YELLOW", row, 3-(++count));
+         S[row][3-count] = 0;
+         sleep(1000);
+      }
+   }
+   setLEDColor(0);
+}
+
+void go()
+{
+   val = 5;
+
+   if(getColorName(c2) == 4)
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting - val);
+      setMotorSpeed(rm, nMotorSpeedSetting + val);
+   }
+   else
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting + val);
+      setMotorSpeed(rm, nMotorSpeedSetting - val);
+   }
+   if(row == 1)
+   {
+      if(getColorName(c1) == 4 && count != 4)
+      {
+         vertex++;
+      }
+      else
+         vertex = 0;
+  }
+  else if(row == 3)
+  {
+     if(getColorName(c1) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+  else if(row == 4)
+  {
+     if(getColorName(c1) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+  else
+  {
+     if(getColorName(c3) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+   // Edit motot speed
+  if(vertex == 1)
+  {
+     if(row == 0 || row == 2)
+     {
+       colorSearchTurn();
+       sleep(150);
+       setMotorSpeed(lm, 25);
+       setMotorSpeed(rm, -25);
+       sleep(80);
+     }
+     else
+     {
+       colorSearchTurn();
+       sleep(150);
+       setMotorSpeed(lm, 10);
+       setMotorSpeed(rm, 12);
+       sleep(80);
+     }
+     patchSave();
+  }
+}
+
+void go_nosave()
+{
+   val = 5;
+
+   if(getColorName(c2) == 4)
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting - val);
+      setMotorSpeed(rm, nMotorSpeedSetting + val);
+   }
+   else
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting + val);
+      setMotorSpeed(rm, nMotorSpeedSetting - val);
+   }
+   if(row == 1)
+   {
+      if(getColorName(c1) == 4 && count != 4)
+      {
+         vertex++;
+      }
+      else
+         vertex = 0;
+  }
+  else if(row == 3)
+  {
+     if(getColorName(c1) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+  else if(row == 4)
+  {
+     if(getColorName(c1) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+  else
+  {
+     if(getColorName(c3) == 4 && count != 4)
+     {
+        vertex++;
+     }
+     else
+        vertex = 0;
+  }
+   // Edit motot speed
+  if(vertex == 1)
+  {
+     if(row == 0 || row == 2)
+     {
+       colorSearchTurn();
+       sleep(150);
+       setMotorSpeed(lm, 12);
+       setMotorSpeed(rm, 10);
+       sleep(80);
+     }
+     else
+     {
+       colorSearchTurn();
+       sleep(150);
+       setMotorSpeed(lm, 10);
+       setMotorSpeed(rm, 12);
+       sleep(80);
+     }
+     count += 1;
+  }
+}
+
+void turnLeft()
+{
+   setMotorSpeed(lm, 25);
+   setMotorSpeed(rm, 25);
+   sleep(250);
+   while(getColorName(c1) > 4)
+   {
+      setMotorSpeed(lm, -nMotorSpeedSetting * 6 / 10);
+      setMotorSpeed(rm, nMotorSpeedSetting * 6 / 10);
+      sleep(10);
+   }
+   while(getColorName(c2) > 4)
+   {
+      setMotorSpeed(lm, -nMotorSpeedSetting * 6 / 10);
+      setMotorSpeed(rm, nMotorSpeedSetting * 6 / 10);
+   }
+   sleep(80);
+   setMotorSpeed(lm, 0);
+   setMotorSpeed(rm, 0);
+   sleep(200);
+   count = 0;
+}
+
+void turnRight()
+{
+   setMotorSpeed(lm, 25);
+   setMotorSpeed(rm, 25);
+   sleep(250);
+   while(getColorName(c3) > 4)
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting * 6 / 10);
+      setMotorSpeed(rm, -nMotorSpeedSetting * 6 / 10);
+      sleep(10);
+   }
+   while(getColorName(c2) > 4)
+   {
+      setMotorSpeed(lm, nMotorSpeedSetting * 6 / 10);
+      setMotorSpeed(rm, -nMotorSpeedSetting * 6 / 10);
+   }
+   sleep(80);
+   setMotorSpeed(lm, 0);
+   setMotorSpeed(rm, 0);
+   sleep(200);
+   count = 0;
+}
+
+void goUp()
+{
+    turnRight();
+    count = 0;
+    while(true)
+    {
+        go_nosave();
+        if (count != 0)
+        {
+            setMotorSpeed(lm, 25);
+            setMotorSpeed(rm, 25);
+            sleep(200);
+            turnLeft();
+            break;
+        }
+    }
+}
+
+void goLeft()
+{
+    count = 0;
+    while(true)
+    {
+        go_nosave();
+        if (count != 0)
+        {
+            break;
+        }
+    }
+    setMotorSpeed(lm, 25);
+    setMotorSpeed(rm, 25);
+    sleep(200);
+}
+
+void completeSearch()
+{
+    while(true)
+    {
+        go();
+        if (count == 3)
+        {
+            if(row == 3)
+            {
+                return;
+            }
+
+            if (row % 2 == 0)
+            {
+                setMotorSpeed(lm,35);
+                setMotorSpeed(rm,30);
+                sleep(200);
+                turnRight();
+            }
+
+            else
+            {
+                setMotorSpeed(lm,30);
+                setMotorSpeed(rm,35);
+                sleep(200);
+                turnLeft();
+            }
+
+            row++;
+
+            if(row%2 == 0)
+            {
+                while(getColorName(c1) == 6)
+                {
+                    go();
+                }
+                colorSearchTurn();
+                      sleep(150);
+                     setMotorSpeed(lm, 10);
+                      setMotorSpeed(rm, 12);
+                      sleep(80);
+
+                count = -1;
+                patchSave();
+
+                setMotorSpeed(lm,30);
+                setMotorSpeed(rm,35);
+                sleep(200);
+                turnLeft();
+            }
+
+            else
+            {
+                while(getColorName(c3) == 6)
+                {
+                    go();
+                }
+                colorSearchTurn();
+                      sleep(150);
+                      setMotorSpeed(lm, 10);
+                    setMotorSpeed(rm, 12);
+                      sleep(80);
+
+                count = -1;
+                patchSave();
+
+                setMotorSpeed(lm, 35);
+                setMotorSpeed(rm, 30);
+                sleep(200);
+                turnRight();
+            }
+            count = 0;
+            sleep(200);
+        }
+    }
+}
+
+void printMap()
+{
+   char map[4][4];
+   for(int i = 0; i < 4; i++)
+   {
+      for(int j = 0; j < 4; j++)
+      {
+         if(S[i][j] == 0)
+         {
+           map[i][j] = '+';
+         }
+         else if(S[i][j] == 1)
+         {
+           map[i][j] = 'O';
+         }
+         else if(S[i][j] == -1)
+         {
+           map[i][j] = 'X';
+         }
+      }
+   }
+   for(int p = 0; p < 4; p++)
+   {
+      for(int q =0; q < 4; q++)
+      {
+         displayStringAt((q+1)*10, 100-(p+1)*10, "%c",map[p][q]);
+      }
+   }
+   sleep(3000);
+}
+
+void printscoreMap()
+{
+   for(int p = 0; p < 4; p++)
+   {
+      for(int q =0; q < 4; q++)
+      {
+         displayStringAt((q+1)*10, 100-(p+1)*10, "%d",dt[p][q]);
+      }
+   }
+   sleep(3000);
+}
+
+void stopMotor()
+{
+   setMotorSpeed(lm, 0);
+   setMotorSpeed(rm, 0);
+}
+
+void movePoint_44()
+{
+   count = 0;
+   while(true)
+   {
+      go_nosave();
+      if (count == 3)
+      {
+         setMotorSpeed(lm, 35);
+         setMotorSpeed(rm, 30);
+         sleep(400);
+         turnLeft();
+         turnLeft();
+         break;
+      }
+   }
+}
+
+task main()
+{
+   while(getButtonPress(1) == 0)
+   {
+      sleep(10);
+   }
+
+   completeSearch();
+   setMotorSpeed(lm,30);
+   setMotorSpeed(rm,30);
+   sleep(350);
+   turnLeft();
+   turnLeft();
+   //sleep(1000);
+
+   sleep(2000);
+
+   movePoint_44();
+   sleep(300);
+   
+   printMap();
+   sleep(3000);
+
+    for(f = 0; f <= 3; f++)
+    {
+      for(e = 0; e<= 3; e++)
+       {
+          if( e == 0 && f == 0) dt[e][f] = S[e][f];
+          else if( f == 0) dt[e][f] = dt[e-1][f] + S[e][f];
+          else if( e == 0) dt[e][f] = dt[e][f-1] + S[e][f];
+          else
+          {
+            if( dt[e-1][f] >= dt[e][f-1])
+            {
+              dt[e][f] = dt[e-1][f] + S[e][f];
+            }
+            else
+              dt[e][f] = dt[e][f-1] + S[e][f];
+          }
+        }
+      }
+
+    printscoreMap();
+    sleep(3500);
+
+    e = 3; f = 3;
+    while(e != 0 && f != 0)
+    {
+      if( dt[e-1][f] >= dt[e][f-1])
+      {
+         goUp();
+         e -= 1;
+      }
+      else
+      {
+      	goLeft();
+      	f -= 1;
+      }
+
+    }
+    if( e == 0 )
+	  {
+	  	while( f != 0)
+	  	{
+	  		goLeft();
+	  		f -= 1;
+	  	}
+    }
+    else
+    {
+    	while( e != 0)
+    	{
+    		goUp();
+    		e -= 1;
+    	}
+    }
+    playTone(240, 20); sleep(200);
+    stopMotor();
+    while(getButtonPress(1) == 0) sleep(10);
+    displayBigTextLine(1, "Score = %d", dt[3][3]);
+    sleep(5000);
+}
